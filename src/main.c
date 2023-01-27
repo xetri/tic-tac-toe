c6a88bd256ac0d233f8c100eafc82c4afa974e80
Avrel <106798140+av-rel@users.noreply.github.com>
2023-01-27 15:09:49 +0545

    1. arrangement of functions in files

--- src/main.c
+++ src/main.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
 
-int main(int argc, char** argv){
+#define SDL_C
+#include "../include/SDL2/SDL.h"
+#include "../include/SDL2/SDL_ttf.h"
 
-    printf("Hello world");
+#include "./start.c"
 
-    return 0;
+int main(int argc, char **argv) {
+
+  if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+    fprintf(stderr, "%s", SDL_GetError());
+    return -1;
+  }
+
+  SDL_Window *window =
+      SDL_CreateWindow(win.name, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                       win.W, win.H, SDL_WINDOW_SHOWN);
+  if (!window) {
+    fprintf(stderr, "%s", SDL_GetError());
+    return -1;
+  }
+
+  SDL_Renderer *renderer =
+      SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+  if (!renderer) {
+    fprintf(stderr, "%s", SDL_GetError());
+    return -1;
+  }
+
+  if (TTF_Init() < 0){
+    fprintf(stderr, "%s", TTF_GetError());
+    return -1;
+  }
+
+  if (SDL_UpdateWindowSurface(window) < 0){
+    fprintf(stderr, "%s", SDL_GetError());
+    return -1;
+  }
+
+  Start(renderer);
+
+  TTF_Quit();
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
 }
---
+++ src/render.c
@@ -0,0 +1,9 @@
+#ifndef SDL_C
+#define SDL_C
+#include "../include/SDL2/SDL.h"
+#include "../include/SDL2/SDL_ttf.h"
+#endif
+
+void render_o(SDL_Renderer *renderer){}
+
+void render_x(SDL_Renderer *renderer){}
