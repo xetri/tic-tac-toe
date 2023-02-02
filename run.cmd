d1b89d53259d81e747fca9b7f31fdb4263f96228
Avrel <106798140+av-rel@users.noreply.github.com>
2023-02-02 18:30:56 +0545

    Update .cmd

--- run.cmd
+++ run.cmd
@@ -1,5 +1,5 @@
 @echo off
 
 if not exist .\out mkdir .\out
-call gcc .\src\main.c -o .\out\main.exe -Wall -Llib\x64 -lmingw32 -lSDL2main -lSDL2
-REM call .\out\main.exe
+call gcc .\src\main.c -o .\out\main.exe -Wall -Llib -lmingw32 -lSDL2main -lSDL2
+call .\out\main.exe
